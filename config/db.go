269a8f8527bc9a7d6b15737871780a4e2bff5bfd
Ruben Gomez <rubncal.9501@gmail.com>
2025-07-10 00:59:25 -0500

    Add env variable sslmode

--- config/db.go
+++ config/db.go
@@ -17,7 +17,8 @@ func InitDB() {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	sslmode := os.Getenv("DB_SSLMODE")
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Database connection failed: %v", err)
